b3c522149769f543c6855346c02554f7abf6a80c
amin sazali <aminhakimsazali@gmail.com>
2022-06-09 21:53:39 +0800

    finish tutoria

---
+++ components/SearchJokes.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="onSubmit">
+        <input type="text" v-model="text"
+        placeholder="Search Jokes...">
+        <input type="submit" value="Search Jokes">
+  </form>
+</template>
+
+<script>
+import { tsImportEqualsDeclaration } from '@babel/types'
+
+export default {
+    name : 'SearchJokes',
+    data(){
+        return {
+            text : ''
+        }
+    },
+    methods : {
+        onSubmit(){
+            this.$emit('search-text', this.text);
+            this.text = ""
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+ 
+ <div>
+<h1>Welcome to the best corny dad jokes on the app</h1>
+ </div>
+  
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+// export default Vue.extend({
+//   name: 'IndexPage',
+// })
+
+
+export default {
+
+    head(){
+        return {
+            title: 'Welcome to dad jokes',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Best place for corny dad jokes'
+                }
+            ]
+        }
+    }
+
+}
+
+</script>
